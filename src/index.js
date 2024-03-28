315f7326fc947d6e3fa8884f0a12d548efd378a5
Neeraj gaur <neerajgaur8448@gmail.com>
2024-03-28 18:45:20 +0530

    adding login/signup funtion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/indexDB.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 4000;
+
+connectDB()
+  .then(() => {
+    app.on("Error", (err) => {
+      console.log("Error:", err);
+      throw err;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.log("MongoDB connsction failed!!! ", err));
 
 // (async () => {
 //   try {
